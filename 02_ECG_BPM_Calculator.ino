7332d27828acf330228c7f82b019d6f0946024fc
Meryem Köksal <meryemkoksal17@gmail.com>
2026-06-02 20:15:12 +0300

    Implement ECG BPM calculator in Arduino sketch

---
+++ 02_ECG_BPM_Calculator.ino
@@ -0,0 +1,75 @@
+  const int pencereboyutu= 10;
+  int datas[pencereboyutu];
+  int index=0;
+  long toplam=0;
+//we write our variables for global if u write in setup, loop wont be able to see ur variables
+  unsigned long sonateszamani=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(13,INPUT);
+  pinMode(12,INPUT);
+  Serial.begin(9600);
+ 
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //security control
+  if(digitalRead(13)==HIGH || digitalRead(12)==HIGH){
+    Serial.println("peds are out of the body, no signal!");
+    delay(10);
+    return; //return cuts the all loop dont continue after this dont go down
+} 
+  int rawsignal=analogRead(A0);
+
+  toplam=toplam-datas[index];
+  //get out the oldest data from the summation
+  datas[index]= rawsignal;
+  //read new data write it instead of oldest data
+  toplam=toplam+datas[index];
+  //add new data to summation
+  index=index+1;
+
+  if(index>= pencereboyutu){
+    index=0;
+  }
+  //for making index <10 
+
+  int ortalama=toplam/pencereboyutu;
+  
+  Serial.print(rawsignal);
+  Serial.print(" ");
+  Serial.println(ortalama);
+  //to continue from next row use ln
+  
+  delay(10);
+
+  int esikDegeri = 340;
+  
+
+  if(ortalama>esikDegeri){
+
+    if(millis()- sonateszamani> 300){
+      unsigned long suankizaman=millis();
+      unsigned long gecensure=suankizaman - sonateszamani;
+
+      int bpm= 60000/gecensure;
+      //for heart pulse calculation
+
+      Serial.print("--PULSE (BPM):");
+      Serial.println(bpm);
+
+      sonateszamani=suankizaman;
+      //update the chronometer
+    }else{
+      Serial.println(0); //if no pulse plotter goes zero
+    }
+    }else{
+      Serial.println(0); //if signal couldnt pass the threshold keep it in zero
+    }
+
+  delay(10);
+}
+
